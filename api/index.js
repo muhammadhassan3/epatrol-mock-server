3c293e2378f063f19fe034c6c0271f0fd8000b96
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-04 12:35:26 +0700

    Change main file location

--- api/index.js
+++ api/index.js
@@ -3,26 +3,26 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 
-const app = express()
+const index = express()
 
 const log = morgan('tiny')
-app.use(cors())
-app.use(log)
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.get("/", (req, res)=>{
+index.use(cors())
+index.use(log)
+index.use(bodyParser.urlencoded({extended: true}))
+index.use(bodyParser.json())
+index.get("/", (req, res)=>{
     res.json({
         error: false,
         message: "Server online"
     })
 })
-app.use("/*", (req, res) => {
+index.use("/*", (req, res) => {
     res.status(404).json({
         error: true,
         message: "Not Found"
     })
 })
 
-app.listen(80, 'localhost', () => {
+index.listen(80, 'localhost', () => {
     console.log("Server started")
 })
